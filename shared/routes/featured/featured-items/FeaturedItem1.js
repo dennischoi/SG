4326e1177c0c1fe2aad39710e37eb66806bc12cf
Dennis Choi <dennis.choi7@gmail.com>
2017-09-13 16:23:17 -0400

    test features

--- shared/routes/featured/featured-items/FeaturedItem1.js
+++ shared/routes/featured/featured-items/FeaturedItem1.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 
 import config from 'utils/config';
 
+// Imgs for first Set items
+// Chairs
+import Chair1 from 'assets/images/Chairs/sg_chair_1.JPG';
+import Chair2 from 'assets/images/Chairs/sg_chair_2.JPG';
+
+
+
+// Desks
+import Desk1 from 'assets/images/Desks/sg_desk_1.JPG';
+import Desk2 from 'assets/images/Desks/sg_desk_2.JPG';
+
+// Counters
+import Counter1 from 'assets/images/Counters/sg_counter_1.JPG';
+import Counter2 from 'assets/images/Counters/sg_counter_2.JPG';
 
 
 import s from './FeaturedItem.scss';
@@ -12,23 +26,42 @@ export default class FeaturedItem1 extends Component {
     return (
       <section> 
         <div className={ s.fItem__section }>
-          <div className={s(s.fItem__gridImgCol, s.fItem__each)}>
+          <div className={s(s.fItem__gridCol, s.fItem__each)}>
+            <div className={s.fItem__content}>
+              <img src={Chair1} className={s.fItem__contentImg} />
+            </div>
             Test
           </div>
-          <div className={s(s.fItem__gridImgCol, s.fItem__each)}>
+          <div className={s(s.fItem__gridCol, s.fItem__each)}>
+            <div className={s.fItem__content}>
+              <img src={Counter1} className={s.fItem__contentImg} />
+            </div>
             Test2
           </div>
-          <div className={s(s.fItem__gridImgCol, s.fItem__each)}>
+          <div className={s(s.fItem__gridCol, s.fItem__each)}>
+            <div className={s.fItem__content}>
+              <img src={Desk1} className={s.fItem__contentImg} />
+            </div>
             Test3
           </div>
 
-          <div className={s(s.fItem__gridImgCol, s.fItem__each)}>
+
+          <div className={s(s.fItem__gridCol, s.fItem__each)}>
+            <div className={s.fItem__content}>
+              <img src={Desk2} className={s.fItem__contentImg} />
+            </div>
             Test4
           </div>
-          <div className={s(s.fItem__gridImgCol, s.fItem__each)}>
+          <div className={s(s.fItem__gridCol, s.fItem__each)}>
+            <div className={s.fItem__content}>
+              <img src={Chair2} className={s.fItem__contentImg} />
+            </div>
             Test5
           </div>
-          <div className={s(s.fItem__gridImgCol, s.fItem__each)}>
+          <div className={s(s.fItem__gridCol, s.fItem__each)}>
+            <div className={s.fItem__content}>
+              <img src={Counter2} className={s.fItem__contentImg} />
+            </div>
             Test6
           </div>
         </div>
