8bb68a889c00b400c7989f10f4455ba9500e1532
Dennis Choi <dennis.choi7@gmail.com>
2017-09-27 10:50:30 -0400

    handler test

--- shared/components/footer/Footer.js
+++ shared/components/footer/Footer.js
@@ -14,9 +14,18 @@ import s from './Footer.scss';
 
 export default class Footer extends Component {
 
+  // Switch of add class JavaScript
   handleClick() {
     console.log('test');
-    $("#test").addClass("class")
+    var element = document.getElementById('test');
+    element.classList.toggle('testClass');
+  }
+
+
+  // Click check id
+  idClick(e) {
+    alert(e);
+
   }
 
 
@@ -25,26 +34,26 @@ export default class Footer extends Component {
 
     return (
       <footer>
-      	<section className={ s.footer__section }>
-      		<div className={ s.footer__tables }>
-      			<h3>Media</h3>
-      			<div className={ s.footer__media__container }>
-      				<div className={ s.footer__media__item }>
-      					<a href="https://www.instagram.com/scout.gather/" className={ s.footer__media__anchor } rel="noopener" target="_blank">
-      						<div className={ s.footer__mediaIcon }>
-      							<FacebookSvg className={ s.footer__media__logoSvg } />
-      						</div>
-      					</a>
-      				</div>
+        <section className={ s.footer__section }>
+          <div className={ s.footer__tables }>
+            <h3>Media</h3>
+            <div className={ s.footer__media__container }>
+              <div className={ s.footer__media__item }>
+                <a href="https://www.instagram.com/scout.gather/" className={ s.footer__media__anchor } rel="noopener" target="_blank">
+                  <div className={ s.footer__mediaIcon }>
+                    <FacebookSvg className={ s.footer__media__logoSvg } />
+                  </div>
+                </a>
+              </div>
 
 
-      				<div className={ s.footer__media__item }>
-      					<a href="https://www.instagram.com/scout.gather/" className={ s.footer__media__anchor } rel="noopener" target="_blank">
-      						<div className={ s.footer__mediaIcon }>
-      							<InstagramSvg className={ s.footer__media__logoSvg } />
-      						</div>
-      					</a>
-      				</div>
+              <div className={ s.footer__media__item }>
+                <a href="https://www.instagram.com/scout.gather/" className={ s.footer__media__anchor } rel="noopener" target="_blank">
+                  <div className={ s.footer__mediaIcon }>
+                    <InstagramSvg className={ s.footer__media__logoSvg } />
+                  </div>
+                </a>
+              </div>
 
               <div className={ s.footer__media__item }>
                 <a href="https://www.instagram.com/scout.gather/" className={ s.footer__media__anchor } rel="noopener" target="_blank">
@@ -63,28 +72,40 @@ export default class Footer extends Component {
               </div>
               
               
-      			</div>
+            </div>
             <div className={ s.footer__test }>
               <div id="test" onClick={(e) => this.handleClick(e)} className={ s.footer__test2 }>
 
               </div>
             </div>
-      		</div>
 
-      		{/*<div className={ s.footer__tables }>
-      			<h3>Test</h3>
-      			<div className={ s.footer__media__container }>
-      				<div className={ s.footer__media__item }>
-      					{this.props.children}
-      				</div>
+            <div className={ s.footer__test }>
+              <div id="test2" onClick={(e) => this.handleClick(e)} className={ s.footer__test2 }>
+
+              </div>
+            </div>
+
+            <div className={ s.footer__test }>
+              <div id="test3" onClick={(e) => this.idClick(this.id)} className={ s.footer__test2 }>
+
+              </div>
+            </div>
+          </div>
+
+          {/*<div className={ s.footer__tables }>
+            <h3>Test</h3>
+            <div className={ s.footer__media__container }>
+              <div className={ s.footer__media__item }>
+                {this.props.children}
+              </div>
 
 
-      				<div className={ s.footer__media__item }>
-      				</div>
-      			</div>
-      		</div>*/}
-      		
-      	</section>
+              <div className={ s.footer__media__item }>
+              </div>
+            </div>
+          </div>*/}
+          
+        </section>
       </footer>
     );
   }
